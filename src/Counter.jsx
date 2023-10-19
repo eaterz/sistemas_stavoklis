9efd953b0dd83374daa3c1971f7216f8d6815d1f
eaterz <muraskoruslans@gmail.com>
2023-10-19 11:35:23 +0300

    commit

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
-
+import "./Counter.css";
 function Counter() {
   const [number, setNumber] = useState(0);
+  const [multiplier, setMultiplier] = useState(1);
 
   function addOne() {
     setNumber(number + 1);
@@ -9,11 +10,29 @@ function Counter() {
   function und() {
     setNumber(number - 1);
   }
+  function multiply() {
+    setNumber(number * multiplier);
+  }
+  function handleChange(reiz) {
+    setMultiplier(reiz.target.value);
+  }
   return (
     <div>
-      <button onClick={addOne}>+ 1</button>
-      <button onClick={und}>- 1</button>
+      <div className="row">
+        <button className="buttonPlus" onClick={addOne}>
+          + 1
+        </button>
+        <button className="buttonMinus" onClick={und}>
+          - 1
+        </button>
+      </div>
+
       <h1>{number}</h1>
+
+      <input type="number" value={multiplier} onChange={handleChange} />
+      <button className="buttonReiz" onClick={multiply}>
+        *
+      </button>
     </div>
   );
 }
